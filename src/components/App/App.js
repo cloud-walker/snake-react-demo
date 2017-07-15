cabf7e8aebce66b4a5612830c15566a4a9e9ef54
Luca Barone <baro.luc@gmail.com>
2017-07-16 01:45:29 +0200

    add layout, background and debug box

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import R from 'ramda'
 
 import grassUrl from './grass.png'
+
+const propToUnit = prop => R.pipe(R.prop(prop), R.multiply(20))
 
 const Content = styled.main`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
 `
 
 const World = styled.div`
   background-image: url(${grassUrl});
   background-repeat: repeat;
-  height: 100px;
-  width: 100px;
+  height: ${propToUnit('height')}px;
+  width: ${propToUnit('width')}px;
+`
+
+const Debug = styled.pre`
+  background-color: yellow;
+  color: brown;
+  border: 0.1em solid;
+  padding: 0.5em;
+  font-weight: bold;
+  
+  h6 {
+    margin: 0;
+  }
 `
 
 const Component = () => (
   <Content>
-    <World>
+    <World height="20" width="20">
       Snake!
     </World>
+    
+    <Debug>
+      <h6>Render's state</h6>
+      
+      {JSON.stringify({}, null, 2)}
+    </Debug>
   </Content>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {injectGlobal} from 'styled-components'
 
 import 'sanitize.css'
 import App from './components/App'
+
+injectGlobal`
+  :root {
+    font-size: 18px;
+  }
+`
 
 ReactDOM.render(<App />, document.getElementById('root'))
