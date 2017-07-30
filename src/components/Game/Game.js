c93b16fdd48dcfc525c5237ce4d0a82f8b1c0481
Luca Barone <baro.luc@gmail.com>
2017-07-30 10:25:17 +0200

    Make the snake move!

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -10,54 +10,124 @@ const grid = Array(WORLD_SIZE).fill(
   Array(WORLD_SIZE).fill(null)
 )
 
+const cells = R.pipe(
+  iReduceToObj(
+    (acc, val, y) => ({
+      ...acc,
+      ...iReduceToObj((acc, val, x) => {
+        const id = `${y}.${x}`
+
+        return {
+          ...acc,
+          [id]: {id, x, y},
+        }
+      })(val),
+    }),
+  ),
+)(grid)
+
+const snake = ['0.0', '0.1']
+const food = getFoodCandidate(cells)
+
 const Component = class extends React.Component {
   static displayName = 'Game'
-  
+
+  get snakeCells() {
+    const {snake, cells} = this.state
+
+    return snake.map(i => cells[i])
+  }
+
+  get foodCell() {
+    return this.state.cells[this.state.food]
+  }
+
+  get worldLimit() {
+    return WORLD_SIZE - 1
+  }
+
   constructor() {
     super()
-    
-    const cells = R.pipe(
-      iReduceToObj(
-        (acc, val, y) => ({
-          ...acc,
-          ...iReduceToObj((acc, val, x) => {
-            const id = `${y}.${x}`
-            
-            return {
-              ...acc,
-              [id]: {id, x, y},
-            }
-          })(val),
-        }),
-      ),
-    )(grid)
-    
-    const snake = ['0.0', '0.1']
-    const food = getFoodCandidate(cells)
-    
-    snake.forEach(x => cells[x] = {
-      ...cells[x],
-      occupied: 'snake',
-    })
-    
-    cells[food] = {
-      ...cells[food],
-      occupied: 'food',
-    }
-    
+    this.frame = this.frame.bind(this)
+
     this.state = {
       cells,
       snake,
       food,
+      direction: 'right',
+    }
+
+    requestAnimationFrame(this.frame)
+  }
+
+  getNextPosition(id) {
+    const [y, x] = R.pipe(
+      R.split('.'),
+      R.map(Number),
+    )(id)
+
+    switch (this.state.direction) {
+      case 'right': {
+        const nextX = x + 1 <= this.worldLimit ? x + 1 : 0
+        return `${y}.${nextX}`
+      }
+      case 'left': {
+        const nextX = x - 1 >= 0 ? x -1 : this.worldLimit
+
+        return `${y}.${nextX}`
+      }
+      case 'top': {
+        const nextY = y - 1 < 0 ? this.worldLimit : y - 1
+
+        return `${x}.${nextY}`
+      }
+      case 'bottom':
+      default: {
+        const nextY = y + 1 > this.worldLimit ? 0 : y + 1
+
+        return `${nextY}.${x}`
+      }
     }
   }
-  
+
+  moveSnake() {
+    const head = R.last(this.snakeCells)
+    const tail = R.head(this.snakeCells)
+
+    const nextCell = this.state.cells[this.getNextPosition(head.id)]
+
+    const nextState = {
+      snake: R.pipe(
+        R.tail,
+        R.append(nextCell.id),
+      )(this.state.snake),
+      cells: R.pipe(
+        R.set(R.lensProp(nextCell.id), nextCell),
+        R.over(R.lensProp(tail.id), R.dissoc('occupied'))
+      )(this.state.cells),
+    }
+
+    /**
+     * Here the move should be safe
+     */
+    this.setState(nextState)
+  }
+
+  frame() {
+    this.acc++
+
+    if (this.acc === 30) {
+      this.acc = 0
+      this.moveSnake()
+    }
+
+    requestAnimationFrame(this.frame)
+  }
+
   render() {
-    const {cells, snake, food} = this.state
-    
     return this.props.children({
-      snake: snake.map(i => cells[i]),
-      food: cells[food],
+      snake: this.snakeCells,
+      food: this.foodCell,
     })
   }
 }
